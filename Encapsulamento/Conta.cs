a120b35e260ab11a3aca3925eb167ee2c8a73552
Lucas da Silva Nunes <lucas.silvaifro@gmail.com>
2024-11-06 22:09:04 -0400

    primeiro commit

---
+++ Encapsulamento/Conta.cs
@@ -0,0 +1,22 @@
+﻿public class Conta
+{
+    private double _Saldo;
+
+
+    public void SetSaldo(double s)
+    {
+        if (s <= 0)
+        {
+            Console.WriteLine("Ta errado meu patrão");
+        }
+        else { _Saldo = s; }
+
+    }
+
+    public double GetSaldo() { 
+
+        return _Saldo;
+    
+    }
+
+}
---
+++ Encapsulamento/Program.cs
@@ -0,0 +1,7 @@
+﻿Conta cont = new Conta();
+Console.WriteLine("Digite o valor que queres depositar:");
+double ss = double.Parse(Console.ReadLine());
+
+cont.SetSaldo(ss);
+
+Console.WriteLine(cont.GetSaldo());
